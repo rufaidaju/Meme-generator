548e8e45737cb35b206916c89913f592181febb9
Rufaida <eng.rufaidajumaa@gmail.com>
2020-09-30 12:05:07 +0200

    Updates

---
+++ precache-manifest.04f38d5ee46ef38f2672e654834314b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af05fcecdcf117051fec1bce97999e64",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "8fdf7cc31d3271c67bd7",
+    "url": "/meme-generator/static/js/2.abf9fef8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.abf9fef8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "09b80c6a8024145b3a2a",
+    "url": "/meme-generator/static/js/main.9071b637.chunk.js"
+  },
+  {
+    "revision": "163fc7ea317d65653242",
+    "url": "/meme-generator/static/js/runtime-main.66d5941a.js"
+  }
+]);
